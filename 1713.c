e48c618cff9e6145ed981d4e9b544ca746e27758
udasuja <udasuja@gmail.com>
2023-10-16 21:43:10 +0900

    FAIL

--- 1713.c
+++ 1713.c
@@ -3,121 +3,111 @@
 
 출력: 당선인의 번호
 
-일단 두가지 배열을 만들고
-하나는 받은 투표의 수
-하나는 순서를 저장한다.
+조건:
+학생들이 추천을 시작하기 전에 모든 사진틀은 비어있다.
+어떤 학생이 특정 학생을 추천하면, 추천받은 학생의 사진이 반드시 사진틀에 게시되어야 한다.
+비어있는 사진틀이 없는 경우에는 현재까지 추천 받은 횟수가 가장 적은 학생의 사진을 삭제하고,
+그 자리에 새롭게 추천받은 학생의 사진을 게시한다. 
+이때, 현재까지 추천 받은 횟수가 가장 적은 학생이 두 명 이상일 경우에는
+그러한 학생들 중 게시된 지 가장 오래된 사진을 삭제한다.
+현재 사진이 게시된 학생이 다른 학생의 추천을 받은 경우에는 추천받은 횟수만 증가시킨다.
+사진틀에 게시된 사진이 삭제되는 경우에는 해당 학생이 추천받은 횟수는 0으로 바뀐다.
 
-첫번째 반복문에 투표를 받고 그 순서를 저장한다.
-(같은 득표수라면 오래된 애가 지워진다.)
-두번째 반복문에는 첫번째 반복문을 토대로 당선인을 뽑는다.
--vote즉 득표수가 0인에가 나오면 continue한다.
--득표수가 0보다 크면 당선인 배열에 0부터 집어넣는다.
-
-세번째 반복문에는 당선인의 배열을 오름차순으로 설정한다.
+사진틀에 즉석으로 넣고
+출력할때는 오름차순으로 출력해야 되는 것 같다.
 */
-
 #include <stdio.h>
+
+typedef struct
+{
+	int vote;//투표수를 저장한다.
+	int number;//투표순서를 저장한다.
+	int people;//학생의 번호를 저장한다.
+}STUDENT;
 
 int main(void)
 {
-	int vote[100]={0}, number[100];
-	int student_m=0;//학생 번호의 최대값을 구한다.
-	int student[20][2]={0};//0열에는 학생의 번호를 1열에는 득표수를 넣는다.
-	int n, vote_paper;
+	int n, total;
+	STUDENT st[20] = { 0 };
 
-	scanf_s("%d %d", &n, &vote_paper);
-	for (int i = 0; i < vote_paper; i++)
+	//처음 입력값은 st배열에 저장하고 투표수와 투표순서 또한 저장한다.
+	//배열의 마지막 요소의 번호는 n-1이 되고 n-1이 될 때까지 입력한 학생의 투표수와 순서를 저장한다.(0부터 n-1까지 차례대로)
+	//만약 n-1까지 다 차면 입력받은 후보와 투표수가 일치하는 것들 중 투표순서가 가장 빠른 것의 자리를 대체한다.
+
+
+	scanf_s("%d %d", &n, &total);
+
+	for (int i = 0; i < total; i++)
 	{
-		int num;
+		int student,k;
+		scanf_s("%d", &student);
 
-		scanf_s("%d", &num);
-		student_m = student_m < num ? num : student_m;
-		//지금 입력받은 학생의 번호가 이전까지의 최대값보다 크면 그것이 학생 번호의 최대값이된다.
-
-		vote[num]++;//num이라는 번호를 갖은 학생의 득표수를 1증가시킨다.
-		number[num] = i;//num이 투표를 받은 순서는 i번째가 되고
-		//나중에 다시 받으면 그때 i로 순서가 초기화된다.
-	}
-	for (int k = 0; k <= student_m; k++)
-	{
-		if (vote[k] == 0)
-		//득표수가 0인 학생이면 그냥 무시한다.
+		if (st[n-1].people==0)
+		//st라는 배열의 n-1까지 후보가 가득 차지 않을 경우
 		{
-			continue;
-		}
-		else
-		{
-			for (int j = 0; j < n; j++)			
+			int zero;
+			for (k = 0; k < n; k++)
 			{
-				if (student[j][1] < vote[k])
-					//j번째 당선인의 득표수보다 k번째 학생의 득표수가 더 많은경우
+				if (st[k].people == student)
+				//st배열에 student라는 번호를 갖은 학생이 존재할 경우
 				{
-					for (int j2 = n - 1; j2 > j; j2--)
-					//student배열의 j번째 요소에 vote배열의 k번째 값을 집어넣어야되므로
-					//뒤로 한칸씩 옮긴다.
-					{
-						student[j2][1] = student[j2 - 1][1];//득표수를 뒤로 한 칸씩 옮긴다.
-						student[j2][0] = student[j2-1][0];//학생의 번호도 뒤로 한 칸씩 옮긴다.
-					}
-					student[j][1] = vote[k];
-					student[j][0] = k;
+					st[k].vote++;
+					st[k].number = i;
 					break;
 				}
-				else if(student[j][1] == vote[k])
-				//student배열에 j번째 학생의 득표수가 k번째 학생이랑 일치할때
+				else if (st[k].people == 0)
+				//st배열에 people 값이 0인 것이 나오면
 				{
-					//j+1번째 학생의 득표수가 k보다 작으면 j번째에 k학생을 쓰고
-					//같으면 다시 j+2번째 학생의 득표수랑 비교한다.
-					while(j<n)
-					//오래된 것을 뒤에다가 배열한다.
-					{
-						int change;
-						//순서가 더 오래된것을 저장한다.
-						if (student[j + 1][1] == vote[k])
-						{
-							if (number[student[j][0]] < number[student[j + 1][0]])
-							//student[j]에 해당하는 학생의 득표순서가 student[j+1]에 해당하는 학생의 득표순서보다 작으면
-							//더 빨리 받은 것이 된다. 즉 더 오래되었다.
-							{
-								change = student[j][0];
-								student[j][0] = student[j+1][0];
-								student[j + 1][0] = change;
-								
-							}
-							j++;
-							//이 조건문 후 다음 반복의 조건중 j+1은 j++을 고려하여 (++j)+1이 된다.
-						}
-//오래된 것을 뒤에다가 놓으면서 student[j+1]학생이 k번째 학생보다 득표수가 작아지면(큰 순서대로 배열하므로 클리는 없다.)
-//가장 오래된 것을 k번째 학생이 대체한다.
-						else
-						{
-							student[j][0] = k;
-							//student[j]가 k학생과 득표수가 같으며 가장 오랜되 애이므로
-							//애를 k번째 학생으로 대체한다.
-							break;
-						}
-					}
-					
+					zero = k;
+					break;
 				}
 			}
-		}
-	}
-	//오름차순을 위한 반복문
-	for (int o = 1; o <= student_m; o++)
-	{
-		
-		for (int o2 = 0; o2 < n; o2++)
-		{
-			
-			if (student[o2][0] == o)
-			//o가 1부터 학생 번호의 최대값까지 커지는데
-			//
-			//student배열에 o2에 o가 존재하면 그것을 출력한다.
+			if (st[k].people!=student)
+			//st[k]가 student라면 st배열에 student가 존재하다는 것이고
+			//그것이 아니라면 st배열에는 student라는 학생의 번호가 없다는 것이다.
 			{
-				printf("%d", o);
-				break;
+				st[zero].people = student;
+				st[zero].vote++;
+				st[zero].number = i;
+			}			
+		}
+		else
+		//가득 찬 경우
+		{
+		//입력받은 학생의 번호가 st배열에 존재하는지 아닌지부터 살펴본다.
+			for (k = 0; k < n; k++)
+			{
+				if (st[k].people == student)
+				//위 조건이 존재하는 경우 k는 n보다 작은 값을 갖는다.
+				{
+					st[k].vote++;
+					st[k].number = i;
+					break;
+				}
+			}
+			if (k == n)
+			//st배열에 student라는 학생의 번호가 없는 경우
+			//없으면 새로 입력받은 것이기에 득표수는 1일 것이다.
+			{
+				int old=i;//이 변수는 득표수가 1이고 투표번호가 가장 빠른 애를 지우기위해 사용된다.
+				for (k = 0; k < n; k++)
+				{
+					if ((st[k].vote==1) && (old > st[k].number))
+					//st배열에 k번째 요소의 학생의 득표수가 1이고 투표순서가 old보다 낮으면
+					//그것이 가장 오래된 것이기에 old에 그것을 저장한다.
+					{
+
+						
+						old = k;//투표받은지 가장 오래된 학생을 저장한다. 
+					}
+				}
+				printf("%d;", k);
+				st[old].people = student;
+				st[old].number = i;
+				printf("%d;%d;",old, st[old].people);
 			}
 		}
 	}
+	printf("%d %d %d", st[0].number, st[1].number, st[2].number);
 	return 0;
 }
