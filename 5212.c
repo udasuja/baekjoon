ae8c3abfb24036c14fa166b1e59c6d5d7732c2a3
udasuja <udasuja@gmail.com>
2023-11-14 23:07:07 +0900

    SUCCESS

--- 5212.c
+++ 5212.c
@@ -37,20 +37,25 @@ int main(void)
 }
 
 void input(int input_row, int input_col)
+//행의 개수와 열의 개수를 입력받는다.
 {
 	int c;
 	for (int i = 1; i <= input_row; i++)
+	//행의 개수만큼 반복
 	{
 		for (int j = 1; j <= input_col; j++)
+		//열의 개수만큼 반복
 		{
 			char str=0;
 			str = getchar();
+			//str의 문자를 저장하고 그 문자가 엔터라면 반복을 종료
 			if (str == '\n')
 			{
 				break;
 			}
 			else
 			{
+				//엔터가 아니라면 i행 j열에 문자 저장한다.
 				arr[i][j] = str;
 			}
 		}
@@ -67,10 +72,13 @@ void sea(int input_row, int input_col)
 		for (int j = 1; j <= input_col; j++)
 		{
 			if (arr[i][j] == 'X')
+			//i행 j열에 저장된 문자가 X라일때 실행
 			{
 				int sea_count = 0;
 				int count = -1;
 				while (count < 2)
+				//상하좌우 . \n 0중 하나이면 둘러싸고 있는 바다의 개수를 1증가 시킨다.
+				//(.은 바다라고 문제에 나왔고 0과 \n는 빈공간(=바다)을 의미한다.)
 				{
 					if (arr[i][j + count] == '.' || arr[i][j + count] == '\n' || arr[i][j + count] == 0)
 					{
@@ -83,12 +91,15 @@ void sea(int input_row, int input_col)
 					count++;
 				}
 				if (sea_count >= 3 )
+				//3면이상이 바다면 그 섬은 잠긴다.
 				{
 
 					fifth_year[i][j] = '.';
 					continue;
 				}
 			}
+			//if문이 실행되지 않으면 continue도 실행되지 않을 것이기에 밑의 코드가 실행된다.
+			//즉 arr의 i행 j열이 fifth_year의 i행 j열에 저장된다.
 			fifth_year[i][j] = arr[i][j];
 		}
 	}
@@ -106,6 +117,8 @@ void fifth_print(int input_row, int input_col)
 		for (int j = 1; j <= input_col; j++)
 		{
 			if (fifth_year[i][j] == 'X')
+			//fifth_year이라는 배열의 i행 j열의 문자가 X라일때
+			//시작 행과 끝행,시작 열과 끝열을 i와 j에 각각 비교하여 시작행,끝행,시작열,끝열을 각각 저장한다.
 			{
 				col.first = col.first > j ? j : col.first;
 				col.last = col.last < j ? j : col.last;
@@ -121,5 +134,6 @@ void fifth_print(int input_row, int input_col)
 		{
 			printf("%c", fifth_year[r][c]);
 		}
+		printf("\n");
 	}
 }
