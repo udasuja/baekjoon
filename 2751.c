c330307c88332b6162ed068ce7638ec6b161eba5
udasuja <udasuja@gmail.com>
2023-12-27 18:44:53 +0900

    SUCCESS

--- 2751.c
+++ 2751.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define NUMBER 1000000
 
 int compare(const void* a, const void* b)
 {
@@ -17,18 +16,21 @@ int compare(const void* a, const void* b)
 int main(void)
 {
 	int n, i;
-	int arr[1000000]={0};
+	int *arr;
 
 	scanf_s("%d", &n);
+	arr = (int*)malloc(sizeof(int) * n);
 	for (i = 0; i < n; i++)
 	{
 		scanf_s("%d", &arr[i]);
 	}
 
-	qsort(arr, (sizeof(arr) - sizeof(int) * n) / sizeof(int), sizeof(int), compare);
+	qsort(arr, n, sizeof(int), compare);//qsort함수는 배열,길이,각 원소의 메모리크기,비교함수를 인자로 갖는데
+	//sizeof(arr)/sizeof(int)라 쓰면 arr의 크기는 *arr의 크기인 4로 나오고 그것을 int의 자료형으로 나누니 값은 1이된다.
+	//그렇기에 길이는 n을 해야 정확하게 qsort함수를 사용가능하다.
 	for (i = 0; i < n; i++)
 	{
-		printf("%d ", arr[i]);
+		printf("%d\n", arr[i]);
 	}
 	return 0;
 }
