d409e0323df19af050b6b289132af9f39bb9b252
udasuja <udasuja@gmail.com>
2024-01-03 23:35:59 +0900

    FAIL and i have no idea

--- 1009.c
+++ 1009.c
@@ -5,6 +5,8 @@ ex)5대 컴퓨터 10개의 데이터=>1번com-1번data,2번com-2번data,3번com-3번data,4번com
 	 데이터의 개수 a,b이때 a는 밑이고 b는 계승이다.
 
 풀이:
+	
+	
 	a를 1부터 b까지 제곱하면서 a의 1의 자리수와 제곱한 수의 1의 자리 숫자가 같아지면 그 횟수를 저장하고 그 횟수와 b를 나누고 나머지를 구한다.
 	그리고 나머지만큼 a를 제곱하면서 a의 1의 자리수를 구한다.
 */
@@ -12,7 +14,7 @@ ex)5대 컴퓨터 10개의 데이터=>1번com-1번data,2번com-2번data,3번com-3번data,4번com
 
 int main(void)
 {
-	int t, a, b,num1,i;
+	int t, a, b, num1, i;
 	scanf_s("%d", &t);
 	while (t--)
 	{
@@ -37,9 +39,8 @@ int main(void)
 					break;
 			}
 		}
-		num1 = a;
 		b -= count;
-		while (b--)
+		while (b-->0)
 		{
 			num1 = num1 * a;
 			if (a >= 10)
@@ -47,6 +48,6 @@ int main(void)
 			else if (a < 10)
 				num1 = num1 % 10;
 		}
-		printf("%d\n", num1 % 10);
+		printf("%d\n", num1 %10 );
 	}
 }
