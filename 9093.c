e5a0412abad6c5e5e27c74771785c4afb6458480
udasuja <119501636+udasuja@users.noreply.github.com>
2024-09-08 18:39:31 +0900

    FIX

--- 9093.c
+++ 9093.c
@@ -1,58 +1,50 @@
 /*
-getchar으로 단어를 하나씩 입력받고 그 단어들을 arr[21]에 순서대로 집어넣는다.
-그러다 스페이스바가 입력되면 reverse함수로 이동한다.
-	reverse(char *arr)
-	int len=strlen(arr)
-	for(i=0;i<len/2;i++)
-		char temp=arr[i]
-		arr[i]=arr[len-i-1]
-		arr[len-i-1]=temp
-reverse함수 이후 맨 위부터 반복한다.
-
-그러다가 '\n'가 입력되면 reverse함수를 실행하고 그 문장입력을 끊낸다.
+전체 문자열을 fgets함수를 통해 입력받고
+공백문자 탐색후 그 공백문자가 있는 위치를 저장하고 문자를 뒤집는다. 
+이를 '\n'가 나올 때까지 반복한다.
 */
-
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <string.h>
-#define SIZE 21
+#define SIZE 1001
 
-void reverse(char* arr_r)
+void reverse(char* start, char* end)
 {
-	int len = strlen(arr_r)-1;
-	int i;
-	for (i = 0; i < len / 2; i++)
+	while (start < end)
 	{
-		char temp = arr_r[i];
-		arr_r[i] = arr_r[len - i - 1];
-		arr_r[len - i - 1] = temp;
+		char temp = *start;//start가 가리키는 주소에 저장된 문자를 temp에 저장
+		*start = *end;//end가 가리키는 주소에 저장된 문자를 start가 가리키는 주소에 저장된 문자와 교환
+		*end = temp;
+		start++;//start가 가리키는 주소를 한 칸 앞으로 이동
+		end--;//end가 가리키는 주소를 한 칸 뒤로 이동
 	}
-	printf("%s", arr_r);
 }
+
 
 int main(void)
 {
 	int n;
 	scanf("%d", &n);
-	getchar();//위 scanf로 인한 널문자 제거
+	getchar();//개행 문자 제거
 
 	while (n--)
 	{
-		char arr[SIZE]={0};
-		char c;
-		int i;
-		for (i=0;i<=SIZE;i++)
+		char arr[SIZE];
+		int i,len;
+		char* start;
+
+		fgets(arr, SIZE, stdin);
+		start = arr;
+		len = strlen(arr);
+		for (i = 0; i < len; i++)
 		{
-			c = getchar();
-			arr[i] = c;
-			if (c == ' '||c=='\n')
+			if (arr[i] == '\n' || arr[i] == ' ')
 			{
-				arr[i+1] = '\0';
-				reverse(arr);
-				i = c=='\n'?SIZE+1:-1;//c가 널문자를 입력받으면 SIZE+1를 i에 저장해 반복을 종료하고 아니면 -1를 저장해 반복을 초기화한다.
+				reverse(start, &arr[i - 1]);//arr[i-1]은 공백문자 이전의 주소값을 전달한다.
+				start = &arr[i + 1];//공백문자 다음으로 start를 설정
 			}
 		}
+		arr[len] = '\0';//fgets함수는 널문자를 포함시키지 않기에 맨 뒤에 널문자 삽입
+		printf("%s", arr);
 	}
-	
 }
