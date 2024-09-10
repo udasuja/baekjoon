19fd7164a97a018c1cf8acd74fcab5711780ef10
udasuja <119501636+udasuja@users.noreply.github.com>
2024-09-10 20:33:43 +0900

    SUCCESS

--- 9012.c
+++ 9012.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,58 +8,69 @@ typedef struct node
 	struct node* link;
 }node;
 
-node* parenthesis(node* list, char s)
+node* parenthesis_open(node* list, char s)
 {
-	if (s == '(')
+	
+	node* new_node = (node*)malloc(sizeof(node));
+	new_node->ch = s;
+	new_node->link = list;//list에는 first가 가리키는 node의 주소값이 저장되어 있다.
+	return new_node;//new_node에 저장된 동적 할당된 node의 주소값을 반환함
+}
+
+node* parenthesis_close(node* list,int *check)
+{
+	if(list!=NULL&&list->ch=='(')
 	{
-		node* new_node = (node*)malloc(sizeof(node));
-		new_node->ch = s;
-		new_node->link = list;//list에는 first가 가리키는 node의 주소값이 저장되어 있다.
-		return new_node;//new_node에 저장된 동적 할당된 node의 주소값을 반환함
-	}
-	else if (s == ')' && list->ch == '(')//first가 가리키는 노드가 (를 저장하고 입력받은 문자가 )일때
-	{
-		node* next_node = list->link;//삭제할 노드가 가리키는 다음 노드의 주소값을 저장함
+		node* temp = list->link;
+		printf("%c\n", list->ch);
 		free(list);
-		return next_node;
+		return temp;
 	}
+	*check = 1;
 	return list;
 }
 
-void remote(node* first)
+void remove_node(node*first,int *check)
 {
-	while (first == NULL)
+	while (first != NULL)
 	{
 		node* temp = first->link;
 		free(first);
 		first = temp;
+		*check = 1;//first가 null이 아니라는 것은 (와)의 개수가 일치하지 않는다는 것이다.
 	}
 }
 
 
 int main(void)
 {
-	node* first = NULL;//node를 가리키는 포인터 변수
 	int n;
 	scanf("%d", &n);
 	getchar();//개행 문자 제거
 	while (n--)
 	{
+		node* first = NULL;//node를 가리키는 포인터 변수
+		int check = 0;
 		char c;
 		while ((c = getchar()) != '\n')
 		{
-			if (c == '(' || c == ')')
+			if (c == '(')
 			{
-				first = parenthesis(first, c);//first가 가리키는 node의 주소값을 전달, 입력받은 문자 c를 전달
+				first = parenthesis_open(first, c);//first가 가리키는 node의 주소값을 전달, 입력받은 문자 c를 전달
+			}
+			else if(c==')')
+			{
+				first = parenthesis_close(first, &check);
 			}
 		}
-		if (first == NULL)
+
+		remove_node(first,&check);
+		if (check==0)
 		{
 			printf("YES\n");
 		}
 		else
 		{
-			remote(first);
 			printf("NO\n");
 		}
 	}
