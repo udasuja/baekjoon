fe3db60945f267c15876c895d8241e0f84da3a94
udasuja <119501636+udasuja@users.noreply.github.com>
2024-12-27 08:02:35 +0900

    SUCCESS(refer to the 17298)

--- 17299.c
+++ 17299.c
@@ -9,7 +9,7 @@ Ai의 오등큰수는 오른쪽에 있으면서
 stack에 들어가고 그 stack의 top과 비교하는 수는 현재의 원소값에 대한 배열이 아닌
 F(Ai)에 대한 배열의 원소와 비교하면 된다.
 
-원소값을 count배열의 index로 하여 그 원소의 F(Ai)값에 접근한다.
+입력 배열의 원소값과 top이 가리키는 stack의 원소값을 count배열의 index로 하여 그 원소의 F(Ai)값에 접근한다. 그리고 둘을 비교한다.
 
 */
 
@@ -19,7 +19,7 @@ F(Ai)에 대한 배열의 원소와 비교하면 된다.
 
 int stack[SIZE];
 int sequ[SIZE];
-int top = 1;
+int top = -1;
 
 int empty(int top)
 {
@@ -30,6 +30,7 @@ int main(void)
 {
 	int n, i;
 	int* count = (int*)calloc(SIZE,sizeof(int));	//모든 원소를 0으로 초기화
+	int s_ind;	//stack에는 현재의 원소 값이 저장되야한다. 
 
 	scanf_s("%d", &n);
 	for (i = 0;i < n;i++)
@@ -39,16 +40,16 @@ int main(void)
 		ind = sequ[i];
 		count[ind]++;	//입력값의 횟수를 센다.
 	}
-	stack[++top] = sequ[n - 1];	//count횟수가 같아도 pop이 실행되므로 맨 마지막 원소는 -1이 출력이된다.
 
 	for (i = n - 1;i >= 0;i--)
 	{
 		int ind=sequ[i];	//i번째 입력값을 ind에 넣어 count의 index로 활용한다.
-		int s_ind = stack[top];	//stack에는 현재의 원소 값이 저장되야한다. 
 		int result;
 
-		while (!empty(top) && count[s_ind] <= count[ind])
+		while (!empty(top))
 		{
+			if (count[s_ind] > count[ind])	//오등큰수를 발견하는 조건
+				break;
 			top--;	//pop연산
 			s_ind = stack[top];	
 		}
@@ -60,15 +61,18 @@ int main(void)
 		}
 		else
 		{
-			result = sequ[i];	//오등큰수
+			result = stack[top];	//오등큰수 (stack의 top은 조건에 맞는 입력 배열의 원소를 가리키므로)
 		}
 
 		stack[++top] = sequ[i];
-		sequ[i] = result;	//어차피 sequ은 사용 안 함으로 결과값을 저장하는 배열로 쓴다.
+		sequ[i] = result;	//어차피 sequ[i]는 stack에 넣어졌으로 sequ는 결과값을 저장하는 배열로 쓴다.
+		s_ind = stack[top];	//stack의 top이 가리키는 원소에 대한 count값을 얻기 위해 stack의 top이 가리키는 원소를 index로 함
 	}
 	for (i = 0;i < n;i++)
 	{
 		printf("%d ", sequ[i]);
 	}
 
+	free(count);
+	return 0;
 }
